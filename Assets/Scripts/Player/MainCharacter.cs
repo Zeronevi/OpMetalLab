41c2b239758ba05f11210aa8abaa04add113e958
Samuel Figueiredo Veronez <83087877+Zeronevi@users.noreply.github.com>
2022-10-05 16:17:06 -0300

    Add Coisos

---
+++ Assets/Scripts/Player/MainCharacter.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class MainCharacter : MonoBehaviour
+{
+    
+    public float speed;
+    Vector2 _velocity;
+    private Rigidbody2D _rb;
+    private bool _canMove = true;
+    
+    void Start()
+    {
+        _velocity = new Vector2();
+        _rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    void Update()
+    {
+        LookAtMouse();
+        if(Input.GetKeyDown(KeyCode.Mouse0)) Shoot();
+        
+        Move();
+    }
+
+    void LookAtMouse()
+    {
+        float angle = (180/math.PI) * math.atan2(   SharedContent.MousePosition.y - transform.position.y, 
+                                                    SharedContent.MousePosition.x - transform.position.x);
+        
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    void Move()
+    {
+        _velocity.x = Input.GetAxisRaw("Horizontal");
+        _velocity.y = Input.GetAxisRaw("Vertical");
+        _velocity = _velocity.normalized;        
+
+        if (_canMove) _rb.velocity = _velocity*speed;
+    }
+    
+    void Shoot(){
+        print("Pew!");
+    }
+}
---
+++ Assets/Scripts/Resources/SharedContent.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharedContent : MonoBehaviour
+{
+    public static Vector2 MousePosition;
+    private Camera _mainCam;
+    
+    private void Start()
+    {
+        _mainCam = Camera.main;
+       MousePosition = new Vector2(); 
+    }
+
+    private void FixedUpdate()
+    {
+        MousePosition = _mainCam.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
