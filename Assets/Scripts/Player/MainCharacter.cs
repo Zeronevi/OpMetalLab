89ad9c584a1680b0fae7898fba8f6e82866045ad
cayquinsqui <cayrinsqui@gmail.com>
2022-10-17 21:32:44 -0300

    Revert "Cone"
    This reverts commit 0ac3c78442904564334639131d87d199a8a10090.

--- Assets/Scripts/Player/MainCharacter.cs
+++ Assets/Scripts/Player/MainCharacter.cs
@@ -7,57 +7,25 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class MainCharacter : MonoBehaviour
 {
-
-    public static Vector3 GetVectorFromAngle(float angle)
-    {
-        float angleRad = angle * (Mathf.PI / 180f);
-        return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
-    }
-
-    public static float GetAngleFormVectorFloat(Vector3 dir)
-    {
-        dir = dir.normalized;
-        float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        if (n < 0) n += 360;
-        return n;
-
-    }
-
-    public static Vector3 GetMouseWorldPosition()
-    {
-        Vector3 MousePosition = Vector3.zero;
-        MousePosition.x = SharedContent.MousePosition.x;
-        MousePosition.y = SharedContent.MousePosition.y;
-
-        return MousePosition;
-    }
-
-
-
+    
     public float speed;
     Vector2 _velocity;
-
     private Rigidbody2D _rb;
     private bool _canMove = true;
-    [SerializeField] private FieldOfView fieldOfView;
     
     void Start()
     {
         _velocity = new Vector2();
         _rb = GetComponent<Rigidbody2D>();
-       
+        
     }
 
     void Update()
     {
         LookAtMouse();
         if(Input.GetKeyDown(KeyCode.Mouse0)) Shoot();
-
         
         Move();
-
-        fieldOfView.UpdatePositionAndDirection(transform.position, GetAimDirection());
-      
     }
 
     void LookAtMouse()
@@ -66,8 +34,6 @@ public class MainCharacter : MonoBehaviour
                                                     SharedContent.MousePosition.x - transform.position.x);
         
         transform.rotation = Quaternion.Euler(0, 0, angle);
-
-
     }
 
     void Move()
@@ -81,14 +47,5 @@ public class MainCharacter : MonoBehaviour
     
     void Shoot(){
         print("Pew!");
-    }
-
-    public Vector3 GetAimDirection()
-    {
-        Vector3 MousePositon = GetMouseWorldPosition();
-        Vector3 CharacterPosition = transform.position;
-
-        Vector3 aimDirection = (MousePositon - CharacterPosition).normalized;
-        return aimDirection;
     }
 }
